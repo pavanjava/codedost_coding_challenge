2446d7e41da7537244cc25bd1b06bcce2280085b
Pavan Mantha <manthapavankumar11@gmail.com>
2023-06-02 15:51:03 +0530

    code to list and dynamic actions on list

---
+++ challenge-1/src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+
+  let [data, setData] = useState(['play cricket', 'play video game', 'read book']);
+  let [checkboxData, setCheckboxData] = useState(Object);
+
+  const deleteItemHandler = (index: number) => {
+    const updatedArr = [...data];
+    if(checkboxData[index]){
+      updatedArr.splice(index, 1);
+      setData(updatedArr);
+    }
+  }
+
+  const handleChckboxClick = (value: boolean, index: number) => {
+    setCheckboxData(checkboxData[index] = value);
+  }
+
+  const createList = (list: string[]): any => {
+    return (
+      <ul>
+        {list.map((item: string, index: number) => (
+          <li key={index}>
+            <input type='checkbox' onChange={(e) => handleChckboxClick(e.target.checked, index)}></input>
+            {item} <button onClick={() => deleteItemHandler(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <div>
+      { createList(data) }
+    </div>
+  );
+}
+
+export default App;
